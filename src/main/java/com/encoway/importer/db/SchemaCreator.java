c833fd44c4a694aec0165578c48e9ba3cc940e4f
koehnee <ekaterina.koehne@encoway.de>
2026-08-19 09:28:38 +0200

    Act 1: Bringing the first data from Excel into the customer app

--- src/main/java/com/encoway/importer/db/SchemaCreator.java
+++ src/main/java/com/encoway/importer/db/SchemaCreator.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+//DDL
 public class SchemaCreator {
     public void createSchema() throws SQLException {
         try (Connection connection = DatabaseConnection.getConnection();
